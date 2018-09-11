e5eefbfd9735e071e7400a8c753c3fbc8fea8095
cmllr <fury224@googlemail.com>
2018-09-11 21:22:26 +0200

    Added battle display, time format

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
             {{ driver.LastName }}
           </span>
           <span :class="'driver-position-meta ' + driver.Status" v-if="driver.Status == 'None'">
-            {{ driver.LapsBehind == 0 ? (driver.Position !== 1 ? round(driver.TimeBehind) : driver.Laps)  : driver.LapsBehind }}
+            {{ driver.LapsBehind == 0 ? (driver.Position !== 1 ? round(driver.TimeBehind) : driver.Laps +1)  : driver.LapsBehind }}
           </span>
           
           <span :class="'driver-position-meta ' + driver.Status" v-if="driver.Status != 'None'">
@@ -23,50 +23,102 @@
         </div>
       </div>
     </div>
+    
+    <div class="battle" v-if="battle !== null">
+      <driver :driver="battle.hunted"></driver>
+      <div class="battle-gap">
+        <div class="battle-gap-arrow">&harr;</div>
+        {{ round(battle.gap) }}
+      </div>
+      <driver :driver="battle.hunter"></driver>
+    </div>
+
     <img class="logo"
          :src="logo" />
     <div class="sector-info" v-if="yellowSector !== null">
       Yellow flag:
       <span class="sector" v-for="(sector, sector_key) in yellowSector" :key="sector_key">Sector {{ sector.replace("Sector State ", "")}}</span>
     </div>
-    <div v-if="urrentDriver !== null"
-         class="driver-info">
-      <div class="driver-info-name">
-        <span class="driver-number"
-              v-html="currentDriver.Number"></span> {{ currentDriver.FirstName }} {{ currentDriver.LastName }}
-      </div>
-      <div class="driver-info-team-name">
-        {{ currentDriver.TeamName }}
-      </div>
+    <div class="current-driver">
+      <driver v-if="currentDriver !== null" :driver="currentDriver"></driver>
     </div>
   </div>
 </template>
 
 <script>
 import store from "./store";
+import driver from "./components/driver";
 import config from "../config"
+import momentjs from "moment"
 
 export default {
+  components: {
+    driver
+  },
   store,
   name: "app",
   data() {
     return {
-      logo: config.logourl
+      logo: config.logourl,
+      battle: null
     };
   },
   created() {
     setInterval(() => {
       this.$store.dispatch("getInfos")
+      this.battle = this.closestBattleForPositionText
     },1000)
   },
   methods: {
     round(value){
       return  Math.round(value * 1000) / 1000
-    }
+    },
+    formatTime(value){
+      // I'm lazy. https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
+      var hours   = Math.floor(value / 3600);
+      var minutes = Math.floor((value - (hours * 3600)) / 60);
+      var seconds = Math.floor(value - (hours * 3600) - (minutes * 60));
+
+      if (hours   < 10) {hours   = "0"+hours;}
+      if (minutes < 10) {minutes = "0"+minutes;}
+      if (seconds < 10) {seconds = "0"+seconds;}
+      return hours+':'+minutes+':'+seconds;
+    },
   },
   computed: {
     currentDriver() {
-      return typeof this.$store.state.infos.Drivers !== 'undefined' ? this.$store.state.infos.Drivers[2] : null;
+      return null;
+      return typeof this.$store.state.infos.Drivers !== 'undefined' ? this.$store.state.infos.Drivers[5] : null;
+    },
+    closestBattleForPosition(){
+      return 5;
+      return this.battlesForPosition.indexOf(Math.min(...this.battlesForPosition));
+    },
+    closestBattleForPositionText(){
+      if (this.closestBattleForPosition == -1){
+        return null
+      } else{
+        return {
+          hunted: this.drivers[this.closestBattleForPosition-1],
+          hunter: this.drivers[this.closestBattleForPosition],
+          gap: this.battlesForPosition[this.closestBattleForPosition]
+        }
+      }
+    },
+    battlesForPosition(){
+      var battles = []
+      this.drivers.forEach((current, index) => {
+        var previous = index !== 0 ? this.drivers[index-1] : null
+        if (previous !== null){
+          var gap = current.TimeBehind
+          if (gap !== 0){
+            battles[index] = gap
+          }
+        } else{
+          battles[0] = Number.MAX_SAFE_INTEGER
+        }
+      });
+      return battles
     },
     drivers() {
       if (typeof this.$store.state.infos.Drivers !== 'undefined'){
@@ -77,7 +129,15 @@ export default {
       return []
     },
     leftDisplayString() {
-      return typeof this.$store.state.infos.Session !== 'undefined'  ? this.$store.state.infos.Session["LeftDisplayString"] : ""
+      if (typeof this.$store.state.infos.Session !== 'undefined'){
+        var session = this.$store.state.infos.Session
+        if (session.MaxLaps === 2147483647) {
+          return this.formatTime(session.CurrentTime) + " /" + this.formatTime(session.MaxTime)
+        } else{
+          return session.CurrentLaps + " /" + session.MaxLaps
+        }
+      }
+      return ""
     },
     yellowSector() {
       if (typeof this.$store.state.infos.Session !== 'undefined'){
@@ -118,16 +178,24 @@ body {
   right: 1em;
   width: 7%;
 }
-.driver-info {
-  background: #323737;
-  color: white;
-  border: 1px solid #323737;
-  width: auto;
-  display: inline-block;
-  position: fixed;
+.current-driver{
+  position: absolute;
   bottom: 5em;
   right: 1em;
+}
+.battle-gap{
+  display: inline-block;
+  width: 5%;
+  text-align: center;
+  padding-left: 2%;
+  padding-right: 2%;
+  font-weight: bold;
+}
+.battle-gap-arrow{
+  color: red;
   font-size: 20pt;
+  font-weight: bolder;
+  text-align: center;
 }
 .sector-info{
   background: #323737;
@@ -146,21 +214,7 @@ body {
   font-weight: bold;
   padding-left: 0.4em;
 }
-.driver-info-name {
-  background: 232323;
-  padding-top: 0.2em;
-  padding-left: 0.4em;
-  padding-right: 0.4em;
-  color: white;
-}
-.driver-info-team-name {
-  padding-left: 0.4em;
-  font-size: 15pt;
-  padding-top: 0.2em;
-}
-.driver-number {
-  margin-right: 0.5em;
-}
+
 .tree {
   position: fixed;
   top: 1em;
@@ -221,5 +275,11 @@ body {
   margin-bottom: 0.7em;
   border-bottom: 1px solid white;
   padding-bottom: 0.2em;
+}
+.battle{
+  position: fixed;
+  bottom: 5em;
+  width: 40%;
+  left: 35%;
 }
 </style>
---
+++ src/components/driver.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="driver-info">
+    <div class="driver-info-name">
+      <span class="driver-number" v-html="number"></span> {{ driver.FirstName }} {{ driver.LastName }}
+    </div>
+    <div class="driver-info-team-name">
+      {{ driver.TeamName }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'driver'
+  ],
+  computed: {
+    number(){
+      return typeof  this.driver !== 'undefined' ? this.driver.NumberFormat.replace("{0}", this.driver.Number ) : "" //TODO: STRING:REPLACE
+    }
+  }
+}
+</script>
+
+<style>
+
+.driver-info {
+  background: #323737;
+  color: white;
+  border: 1px solid #323737;
+  width: 300px;
+  display: inline-block;
+  font-size: 20pt;
+}
+.driver-info-name {
+  background: 232323;
+  padding-top: 0.2em;
+  padding-left: 0.4em;
+  padding-right: 0.4em;
+  color: white;
+}
+.driver-info-team-name {
+  padding-left: 0.4em;
+  font-size: 15pt;
+  padding-top: 0.2em;
+}
+.driver-number {
+  margin-right: 0.5em;
+}
+</style>
